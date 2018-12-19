2634e51a95dc095c2836fb91119b53097c342d16
Andrey <andrey@mikhailichenko.su>
2018-12-19 11:29:05 +0300

    INIT

---
+++ client/app/core/router.js
@@ -0,0 +1,27 @@
+"use strict";
+
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+import Home from "../modules/home";
+import Counter from "../modules/counter";
+import Devices from "../modules/devices";
+import Posts from "../modules/posts";
+import Profile from "../modules/profile";
+import Projects from "../modules/projects";
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+	mode: "hash",
+	routes: [
+		{ path: "/", component: Home },
+		{ path: "/devices", component: Devices },
+		{ path: "/posts", component: Posts },
+		{ path: "/counter", component: Counter },	
+		{ path: "/profile", component: Profile },
+		{ path: "/projects", component: Projects }
+		// { path: "/users", component: User, meta: { needRole: "admin" } },
+		//{ path: "*", component: NotFound }
+	]
+});
